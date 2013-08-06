41163dd55f4e6300a3d64d66c40851772e1968f7
marcus <marcus85w@hotmail.com>
2013-08-06 20:52:33 +0200

    Prototype of new gui event system.

--- guiitem.h
+++ guiitem.h
@@ -2,6 +2,7 @@
 #define GUIITEM_H
 
 #include "guitypedefs.h"
+#include "guievent.h"
 
 #include <mw/signal.h>
 
@@ -38,7 +39,7 @@ namespace gui {
 		}
 
 		virtual void draw() {
-		}	
+		}
 
 		int getWidth() const {
 			return width_;
@@ -73,7 +74,7 @@ namespace gui {
 		void setWidth(int width) {
 			width_ = width;
 		}
-
+		
 		void setHeight(int height) {
 			height_ = height;
 		}
@@ -92,7 +93,7 @@ namespace gui {
 
 		void click() {
 			onClick_(this);
-		}	
+		}
 
 	private:
 		void eventUpdate(const SDL_Event& windowEvent) {
