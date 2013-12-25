c44c3a50e65f28241aa05eb92b51d25d98772d57
mwthinker <mwthinker@yahoo.se>
2013-12-26 00:45:22 +0100

    Fixed gamecomponent!

--- src/gamecomponent.h
+++ src/gamecomponent.h
@@ -12,7 +12,22 @@ public:
 	}
 
 	void draw(float deltaTime) override {
+		glPushMatrix();
+		gui::Dimension dim = getSize();
+
+		// Centers the game and holds the correct proportions. The sides is transparent.
+		if (tetrisGame_.getWidth() / dim.width_ > tetrisGame_.getHeight() / dim.height_) {
+			// Black sides, up and down.
+			double scale = (double) dim.width_ / tetrisGame_.getWidth();
+			glScaled(scale, scale, 1);
+		} else {
+			// Black sides, left and right.
+			double scale = (double) dim.height_ / tetrisGame_.getHeight();
+			glScaled(scale,scale,1);
+		}
+
 		tetrisGame_.draw();
+		glPopMatrix();
 	}
 
 private:
