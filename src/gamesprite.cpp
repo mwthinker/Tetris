8c9d4ba8c9f6ada96fab0df8c361c0ef75ea4ef7
mwthinker <mwthinker@yahoo.se>
2014-03-29 13:30:33 +0100

    Updated submodules.
    Textures are inverted inorder to have the lower left point correspond to
    the OpenGLs dafault orientation.

--- src/gamesprite.cpp
+++ src/gamesprite.cpp
@@ -4,14 +4,14 @@ const mw::Sprite spriteBackground("images/background.png");
 
 const mw::Texture textureSquares("images/tetris.png");
 
-const mw::Sprite spriteMan(textureSquares, 64, 64, 64, 64);
-const mw::Sprite spriteComputer(textureSquares, 448, 0, 64, 64);
-const mw::Sprite spriteCross(textureSquares, 0, 64, 64, 64);
+const mw::Sprite spriteMan(textureSquares,       64, 1, 62, 62);
+const mw::Sprite spriteComputer(textureSquares, 448, 65, 62, 62);
+const mw::Sprite spriteCross(textureSquares,      1, 1, 62, 62);
 
-const mw::Sprite spriteI(textureSquares, 193, 1, 62, 62);
-const mw::Sprite spriteJ(textureSquares, 129, 1, 62, 62);
-const mw::Sprite spriteL(textureSquares, 257, 1, 62, 62);
-const mw::Sprite spriteO(textureSquares, 385, 1, 62, 62);
-const mw::Sprite spriteS(textureSquares, 65, 1, 62, 62);
-const mw::Sprite spriteT(textureSquares, 321, 1, 62, 62);
-const mw::Sprite spriteZ(textureSquares, 1, 1, 62, 62);
+const mw::Sprite spriteI(textureSquares, 193, 65, 62, 62);
+const mw::Sprite spriteJ(textureSquares, 129, 65, 62, 62);
+const mw::Sprite spriteL(textureSquares, 257, 65, 62, 62);
+const mw::Sprite spriteO(textureSquares, 385, 65, 62, 62);
+const mw::Sprite spriteS(textureSquares,  65, 65, 62, 62);
+const mw::Sprite spriteT(textureSquares, 321, 65, 62, 62);
+const mw::Sprite spriteZ(textureSquares,   1, 65, 62, 62);
