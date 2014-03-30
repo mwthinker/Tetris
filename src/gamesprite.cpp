0e9e663e9c51f36d59f40da8757843089b0489c6
mwthinker <mwthinker@yahoo.se>
2014-03-30 18:25:27 +0200

    Tetris squares image updated.

--- src/gamesprite.cpp
+++ src/gamesprite.cpp
@@ -1,17 +1,19 @@
 #include "gamesprite.h"
 
 const mw::Sprite spriteBackground("images/background.png");
-
 const mw::Texture textureSquares("images/tetris.png");
 
-const mw::Sprite spriteMan(textureSquares,       64, 1, 62, 62);
-const mw::Sprite spriteComputer(textureSquares, 448, 65, 62, 62);
-const mw::Sprite spriteCross(textureSquares,      1, 1, 62, 62);
+// Top bar.
+const mw::Sprite spriteComputer(textureSquares,   1, 1, 62, 62);
+const mw::Sprite spriteCross(textureSquares,     65, 1, 62, 62);
+const mw::Sprite spriteMan(textureSquares,      129, 1, 62, 62);
+const mw::Sprite spriteZoom(textureSquares,     193, 1, 62, 62);
 
-const mw::Sprite spriteI(textureSquares, 193, 65, 62, 62);
+// Tetris squares.
+const mw::Sprite spriteZ(textureSquares, 1, 65, 62, 62);
+const mw::Sprite spriteS(textureSquares, 65, 65, 62, 62);
 const mw::Sprite spriteJ(textureSquares, 129, 65, 62, 62);
+const mw::Sprite spriteI(textureSquares, 193, 65, 62, 62);
 const mw::Sprite spriteL(textureSquares, 257, 65, 62, 62);
-const mw::Sprite spriteO(textureSquares, 385, 65, 62, 62);
-const mw::Sprite spriteS(textureSquares,  65, 65, 62, 62);
 const mw::Sprite spriteT(textureSquares, 321, 65, 62, 62);
-const mw::Sprite spriteZ(textureSquares,   1, 65, 62, 62);
+const mw::Sprite spriteO(textureSquares, 385, 65, 62, 62);
--- src/gamesprite.h
+++ src/gamesprite.h
@@ -5,18 +5,21 @@
 #include <mw/sprite.h>
 
 extern const mw::Sprite spriteBackground;
-
 extern const mw::Texture textureSquares;
-extern const mw::Sprite spriteMan;
+
+// Top bar.
 extern const mw::Sprite spriteComputer;
 extern const mw::Sprite spriteCross;
+extern const mw::Sprite spriteMan;
+extern const mw::Sprite spriteZoom;
 
-extern const mw::Sprite spriteI;
-extern const mw::Sprite spriteJ;
-extern const mw::Sprite spriteL;
-extern const mw::Sprite spriteO;
-extern const mw::Sprite spriteS;
-extern const mw::Sprite spriteT;
+// Tetris squares.
 extern const mw::Sprite spriteZ;
+extern const mw::Sprite spriteS;
+extern const mw::Sprite spriteJ;
+extern const mw::Sprite spriteI;
+extern const mw::Sprite spriteL;
+extern const mw::Sprite spriteT;
+extern const mw::Sprite spriteO;
 
 #endif // GAMESPRITE_H
